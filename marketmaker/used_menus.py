a45f7eadd22e69bb9a1d0a006ed01a824ebb5925
KevinHZhao <65431430+KevinHZhao@users.noreply.github.com>
2025-02-18 17:22:19 -0500

    Added pages for used command (#12)

---
+++ marketmaker/used_menus.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import menus
+from discord.ext.menus import button, First, Last
+
+class MyMenuPages(menus.MenuPages, inherit_buttons=True):
+    @button('⏮️', position=First(0))
+    async def go_to_first_page(self, payload):
+        await self.show_page(0)
+
+    @button('◀️', position=First(1))
+    async def go_to_previous_page(self, payload):
+        await self.show_checked_page(self.current_page - 1)
+
+    @button('▶️', position=Last(1))
+    async def go_to_next_page(self, payload):
+        await self.show_checked_page(self.current_page + 1)
+
+    @button('⏭️', position=Last(2))
+    async def go_to_last_page(self, payload):
+        max_pages = self._source.get_max_pages()
+        last_page = max(max_pages - 1, 0)
+        await self.show_page(last_page)
+
+    # @button('⏹️', position=Last(0))
+    # async def stop_pages(self, payload):
+    #     self.stop()
+
+class MySource(menus.ListPageSource):
+    async def format_page(self, menu, entries):
+        embed = discord.Embed(
+            description=entries, 
+            color=discord.Colour.random()
+        )
+        embed.set_footer(text=f"Requested by {menu.ctx.author}")
+        return embed

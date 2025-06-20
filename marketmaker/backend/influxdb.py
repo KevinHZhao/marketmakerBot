908c10f43772d5225a3cecec50d1b195145bb5a7
Mingde Yin <mingdeyin@gmail.com>
2025-06-20 01:08:44 -0400

    secondary y axis (#23)

--- marketmaker/backend/influxdb.py
+++ marketmaker/backend/influxdb.py
@@ -51,6 +51,18 @@ def plotter(results, item: str):
     plt.xlabel('Time')
     plt.ylabel('Amount')
     plt.grid()
+
+    # secondary axes -- % change
+    starting_amount = df['amount'][0]
+    primary_ax = plt.gca()
+    secondary_ax = primary_ax.twinx()
+
+    y1, y2 = primary_ax.get_ylim()
+    y1_ratio = (y1 / starting_amount - 1) * 100
+    y2_ratio = (y2 / starting_amount - 1) * 100
+    secondary_ax.set_ylim(y1_ratio), y2_ratio) 
+
+    secondary_ax.set_ylabel("% Change")
     
     # Save the plot to a BytesIO object
     img_buffer = BytesIO()
